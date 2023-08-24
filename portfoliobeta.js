cdc3c462c23e5b5023071c7638cf9313d1d89bc8
univ <f.lewis20.class18@outlook.com>
2023-08-24 05:12:20 +0000

    portfoliobeta.js

--- portfoliobeta.js
+++ portfoliobeta.js
@@ -38,12 +38,57 @@ function dateAgenda() {
 }
 
 var doGet = function (e) {
-  const Route = {};
-  Route.path = function (route, callback) {
-    Route[route] = callback;
-  };
-  Route.path("default", website);
-  return Route["default"](e);
+  var foobarr = e.parameter["func"] || "renderFile";
+  var libName = "boilerplate";
+  var libFunc = foobarr;
+  var rndPage = [
+    `index proMedia epaWebsite callBack oddChances jsGame checkOnDay uiAccess popUpOpen congressLeg congressMembers jFundamentals gnuFree myGNUFreeJS`,
+  ]
+    .toString()
+    .split(" ")[
+    Math.floor(
+      Math.random() *
+        Math.floor(
+          [
+            `index proMedia epaWebsite callBack oddChances jsGame checkOnDay uiAccess popUpOpen congressLeg congressMembers jFundamentals gnuFree myGNUFreeJS`,
+          ]
+            .toString()
+            .split(" ").length,
+        ),
+    )
+  ];
+  args = e.parameter["args"] || ["jFundamentals"];
+  return renderTemplate(
+    this[libName].contentApp(`<?!= appL ?>`, {
+      appL: this[libName][
+        foobarr ||
+          HtmlService.createHtmlOutput(
+            `
+              <script>
+                document.getElementById("appList").value
+              </script>
+              `,
+          ).getContent()
+      ].apply(this, [
+        args ||
+          HtmlService.createHtmlOutput(
+            `
+              <script>
+                document.getElementById("username").value
+              </script>
+              `,
+          ).getContent(),
+      ]),
+    }),
+    { e: e },
+  );
+};
+
+var runBoilerplate = function (func, args) {
+  var libName = "boilerplate";
+  var libFunc = func || "doGet";
+  args = args || [];
+  return this[libName][libFunc].apply(this, args);
 };
 
 var runAll = function (func, args) {
@@ -81,6 +126,6 @@ var sidebarMenu = function onOpen() {
 };
 
 var website = function site(e) {
-  const webApp = boilerplate.doGet(e);
+  const webApp = boilerplate.proMediaSnip(e);
   return webApp;
 };
