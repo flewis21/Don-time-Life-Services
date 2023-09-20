b05baa092d4ba3c7581f8bcacd084782faff13ac
univ <f.lewis20.class18@outlook.com>
2023-09-20 15:29:24 +0000

    .

--- render.js
+++ render.js
@@ -27,9 +27,9 @@ var renderTemplate = function (blob, argsObject) {
     });
   }
   var funcCheck = appList();
-  var schedule = foo.dateTime(new Date());
+  var schedule = boilerplate.dateTime(new Date());
   // var research = geneFrame(seoSheet(coUtility()[0].rndTitle).url)
-  var html = foo.contentApp(
+  var html = boilerplate.contentApp(
     `
   <html id="renderTemplate">
     <head>
@@ -53,13 +53,33 @@ var renderTemplate = function (blob, argsObject) {
     <script src="https://cdnjs.cloudflare.com/ajax/libs/luxon/3.0.1/luxon.min.js" integrity="sha512-6ZJuab/UnRq1muTChgrVxJhSgygmL2GMLVmSJN7pcBEqJ1dWPbqN9CiZ6U3HrcApTIJsLnMgXYBYgtVkJ8fWiw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script type="text/javascript" src="https://unpkg.com/tabulator-tables@5.2.3/dist/js/tabulator.min.js"></script>
     <style>
-    * {box-sizing: border-box;margin: 0;
-      padding: 0;font-family: "Roboto", sans-serif;}
+    * {
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+      font-family: "Roboto", sans-serif;}
     html,
     body {height: 100%;}
     main {height: 92%;
           margin-top: 10px;}
-    body {background-color: #ffc107;}
+    body {
+      background-color: silver;
+      border: solid .5px;
+      margin: 10px 15px 5px 15px;
+      box-shadow: 3px 3px 2px rgb(3,3,3);
+      flex-grow: 1;
+      color:blue;
+      text-decoration:bold;
+      flex-flow: row wrap;
+      grid-column: 1;
+      grid-row: 1;
+      font-size: 2rem;
+      padding:5px;
+      text-align: center;
+      align-content: flex-start;
+      z-index: -5;
+      height: 100%;
+      overflow: auto;}
     header {background-color: rgba(255,255,255,.1);
             color: #a7e1ee;
             font-size: smaller;}
@@ -145,7 +165,20 @@ var renderTemplate = function (blob, argsObject) {
                 text-align: center;
                 user-select: none;}
     .menu-item:hover>.menu-img {transform: scale(1.03);}
-    img {width: 160px;}</style></head>
+    img {width: 160px;}
+    a:link, a:visited {
+      color:black !important;
+      text-decoration:none !important;
+      flex-flow: row wrap !important;
+      grid-column: 1 !important;
+      grid-row: 1 !important;
+      align-content: flex-start !important;
+      z-index: 0 !important;
+      height: 100% !important;
+      overflow: auto !important;}
+    a:hover, a:active{ 
+      color:pink  !important;
+      text-decoration:none  !important;}</style></head>
     <body>
     <? var invArray = ["group bank semi fact bio science block chain space coin"] ?>
     <? var calcArray = ["0 1 2 3 4 5 6 7 8 9"].toString().split(" ") ?>
@@ -164,50 +197,6 @@ var renderTemplate = function (blob, argsObject) {
   <div id="calculatediv"></div>
   <div id="investdiv"></div>
   <div id="rndnewdiv"></div>
-    <script>
-
-      document.addEventListener("DOMContentLoaded", homeW3) 
-      function homeW3() {
-      function serverside(func, args) {
-        return new Promise((resolve, reject) => {
-          google.script.run
-          .withSuccessHandler((result) => {
-              resolve(result)})
-          .withFailureHandler((error) => {
-              console.log(error)
-              console.log(document.getElementById("test").innerHTML)
-              reject(error)})
-          .runBoilerplate([func], [args])})};
-        
-        const htmlStructure = document.getElementById("divSEOC").innerHTML
-        const results = document.getElementById("w3Search")
-        const strValue = results.value
-        results.addEventListener("change",(e) => 
-        {
-
-            var cap = e.target.value
-            document.getElementById("w3Search").value = ""
-            document.getElementById("divSEOC").innerHTML = "... waiting for " + cap
-            serverside("portBing", cap)
-            .then((vid) => {
-
-            
-              if (vid) {
-
-                    
-                      document.getElementById("divSEOC").innerHTML = htmlStructure
-                      document.getElementById("w3Res").src = vid;}
-              })
-            .catch((er) => {
-
-            
-              console.log(er)
-              document.getElementById("divSEOC").innerHTML = JSON.stringify(er)
-              })
-
-            
-        })}
-    </script>
     <script>
       function aboutMeSearch() {
            function serverside(func, args) {
@@ -401,7 +390,7 @@ var renderTemplate = function (blob, argsObject) {
     .append(html)
     .append(schedule)
     .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
-    .setTitle("Don'time Life Services");
+    .setTitle("Wild Sage Brush");
 };
 
 var appList = function (e) {
@@ -416,22 +405,23 @@ var appList = function (e) {
       <link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet">
     </head>
     <body>
-    <? var appList = foo.appSort(9) ?>
+    <? var appList = boilerplate.appSort(9) ?>
     <? var dropList = appList.map((item) => {
         return "<option>" + item + "</option>"
 
     }).join("") ?>
     <? var result = JSON.stringify(dropList) ?>
-    <? var appUrl = foo.getUrl(ScriptApp) + "?func=" ?>
+    <? var appUrl = boilerplate.getUrl(ScriptApp) + "?func=" ?>
+    <div class="container">
     <nav>
       <div class="row">
         <div class="col s10 card-panel l12 m12 push-s1">
           <div class="z-depth-5 green toolbar_icon toolbar_iconHover container">
             <div class="col s12 l12 m12">
-              <div class="black" id="seoData">
+              <div class="black" id="seoCoData">
                 <div class="row">
                   <div class="col s10 l10 m10 card-panel push-s1 push-l1 push-m1">
-                    <div class="container row valign-wrapper"><?!= foo.rule() ?></div>
+                    <div class="container row valign-wrapper"><?!= boilerplate.rule() ?></div>
                       <div id="indexDiv" class="video-container grey flow-text" style="clear: both;overflow-y: auto;overflow-x: hidden;text-align: center">
                         <div class="col s10 l10 m10 receipt black darken-1">
                           <iframe 
@@ -454,9 +444,11 @@ var appList = function (e) {
                 <div class="row">
                   <div class="col s10 card-panel l12 m12 push-s1">
                     <div class="z-depth-5 grey toolbar_icon toolbar_iconHover container">
+                    <div id="w3Div">
                       <div class="col s12 l12 m12">
                         <input style="font-size:18pt;color:green" placeholder="research" class="timepicker flow-text menu-img z-depth-5 card-panel black scale-transition scale-out scale-in receipt btn-large" id="homeIndex"  type="search" />
                       </div>
+                    </div>
                     </div>
                   </div>
                 </div>
@@ -466,7 +458,8 @@ var appList = function (e) {
         </div>
       </div>
     </nav>
-      <div class="row">
+    </div>
+      <div class="row container">
         <div class="col s12 m12 l12 menu z-depth-5 card-panel amber scale-out scale-in" style="font-size: 30px">
           <div class="container">
             <div class="col s12 m12 l12 receipt nav-wrapper deep-purple darken-1">
@@ -494,62 +487,82 @@ var appList = function (e) {
                 console.log(document.getElementById("test").innerHTML)
                 reject(error)})
             .runBoilerplate([func], [args])})};
-          
-          const htmlStructure = document.getElementById("indexDiv").innerHTML
+
+          const docWnd = document.getElementById("indexDiv")
+          const w3Wnd = document.getElementById("w3Div")
           const results = document.getElementById("homeIndex")
-          const strValue = results.value
-          if (!strValue) {
-
-            document.getElementById("indexDiv").innerHTML = "... Loading"
-            serverside("dtlsPict")
-            .then((stream) => {
-
-              if (stream) {
-
-                document.getElementById("indexDiv").innerHTML = htmlStructure
-                document.getElementById("indexRes").src = stream
-
-              }
-              else {
-
-                document.getElementById("indexDiv").innerHTML =  JSON.stringify(er)
-
-              }
-
-            })
-            .catch((er) => {
-
-              console.log(er)
-              document.getElementById("indexDiv").innerHTML = JSON.stringify(er)
-
-            })
-
-          }
           results.addEventListener("change",(e) => 
           {
 
               var cap = e.target.value
+              if (cap) 
+              try {
+
+            
               document.getElementById("homeIndex").value = ""
-              document.getElementById("indexDiv").innerHTML = "... waiting for " + cap
-              serverside("stockHistory", cap)
+              const htmlStructure = docWnd.innerHTML
+              docWnd.innerHTML = "... waiting for " + cap
+              serverside("generalWorkInvoice", cap)
               .then((vid) => {
 
               
-                if (vid) {
+                if (vid.length === 99) {
 
-                      
+                  
+
+                      console.log(typeof vid + " with length = " + vid.length)
                         // User clicked "No" or X in the title bar.
-                        document.getElementById("indexDiv").innerHTML = htmlStructure
-                        document.getElementById("indexRes").src = vid;}
+                        // docWnd.innerHTML = htmlStructure
+                        // document.getElementById("indexRes").src = vid;
+                            // console.log(vid)
+                        var linkFollow = document.createElement("a");
+                        linkFollow.href = vid
+                        linkFollow.id = "linkFOLLOW";
+                        linkFollow.target = "_child";
+                        document.body.appendChild(linkFollow);
+                      document.getElementById("linkFOLLOW").click();
+                      document.getElementById("linkFOLLOW").remove();
+                        
+
+                        }
+                  else {
+
+                        console.log(typeof vid + " with length = " + vid.length)
+                        // User clicked "No" or X in the title bar.
+                        docWnd.innerHTML = vid;
+                    
+                  }
                 })
               .catch((er) => {
 
               
                 console.log(er)
-                document.getElementById("indexDiv").innerHTML = JSON.stringify(er)
-                })
+                docWnd.innerHTML = JSON.stringify(er)
+                })}
 
-              
+            catch   {
+
+            const htmlStructure = docWnd.innerHTML
+            const w3Structure = w3Wnd.innerHTML
+            docWnd.innerHTML = "... Loading"
+            w3Wnd.innerHTML = ""
+            serverside("dtlsPict")
+            .then((stream) => {
+
+
+                docWnd.innerHTML = htmlStructure
+                w3Wnd.innerHTML = w3Structure
+                document.getElementById("indexRes").src = stream
+
+            })
+            .catch((er) => {
+
+              console.log(er)
+              docWnd.innerHTML = JSON.stringify(er)
+
+            })
+
+            }
           })}
       </script>
       <script>
@@ -568,18 +581,29 @@ var appList = function (e) {
         var bar = document.getElementById("uiApp").value || "";
         document.getElementById("uiApp").value = ""
 
-        serverside("stockHistory", bar)
+        serverside("generalWorkInvoice", bar)
         .then((cChange) => {
-            if (cChange) {
+          console.log(typeof cChange + " with length = " + cChange.length)
+            if (cChange.length === 99) {
+
+              
               // console.log(cChange)
           var linkFollow = document.createElement("a");
-          linkFollow.href = cChange;
+          linkFollow.href = cChange
           linkFollow.id = "linkFOLLOW";
           linkFollow.target = "_child";
           document.body.appendChild(linkFollow);
         document.getElementById("linkFOLLOW").click();
-        document.getElementById("linkFOLLOW").remove()}
-                
+        document.getElementById("linkFOLLOW").remove();
+
+        }
+        else {
+          var docWnd = document.getElementById("seoCoData")
+          const w3Wnd = document.getElementById("w3Div")
+            docWnd.innerHTML = cChange;
+            w3Wnd.innerHTML = ""
+
+        }      
         })
         .catch((er) => {
 
